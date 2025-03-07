23f712368524de748465a023cec1181ae408aa9d
mohammedmeski200 <mohammedmeski200@gmail.com>
2025-03-07 16:12:15 +0000

    react is working with tailwindcss

---
+++ resources/js/Pages/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div className=''>Register</div>
+  )
+}
+
+export default Register
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return inertia('Home');});
+Route::get('/login', function () {return inertia('Login');});
+Route::get('/register', function () {return inertia('Register');});
